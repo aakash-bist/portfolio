5ad046a3527879213f8cf20fe232f8e6210ca7ca
aakash <aakash@etech-services.com>
2021-05-17 01:13:44 +0530

    updated particles interactivity

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -33,19 +33,45 @@ function Particle() {
           },
         },
         interactivity: {
+          detect_on: "window",
           events: {
-            onclick: {
+            onhover: {
               enable: true,
-              mode: "push",
+              mode: "repulse"
             },
+            onclick: {
+              enable: false,
+              mode: "bubble"
+            },
+            resize: true
           },
           modes: {
-            push: {
-              particles_nb: 1,
+            grab: {
+              distance: 400,
+              line_linked: {
+                opacity: 1
+              }
             },
-          },
+            bubble: {
+              distance: 400,
+              size: 40,
+              duration: 2,
+              opacity: 8,
+              speed: 3
+            },
+            repulse: {
+              distance: 100,
+              duration: 0.4
+            },
+            push: {
+              particles_nb: 4
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
         },
-        retina_detect: true,
+        retina_detect: true
       }}
     />
   );
