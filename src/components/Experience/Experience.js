647a2da26f72d061315222a6ad7e640840810f6b
aakash <aakash@etech-services.com>
2021-05-16 20:24:39 +0530

    updated

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -6,70 +6,96 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 
 import { col, Badge } from "react-bootstrap";
-import "../../style.css";
+import "../../Assets/CSS/Experience.css";
+import loopback from "../../Assets/icons/loopback.png";
 
 const Experience = () => {
   const resumeExperience = [
     {
+      company: "E-Tech Service Pvt. Ltd.",
+      position: "MEAN Stack Developer",
+      icon: "devicon-angularjs-plain",
+      timePeriod: "1.2020 - Present",
+      mainTechStack: ["Angular 6,7,8", "LoopBack 3.x"],
+      technologies: ["Angular 8", "LoopBack 3.x", "TypeScript"],
+    },
+    {
       company: "Planify Consultancy India Pvt. Ltd.",
-      title: "Front-End Developer",
+      position: "Front-End Developer",
+      image: loopback,
       timePeriod: "8.2019 - 11.2019",
       mainTechStack: ["Angular 6"],
       technologies: ["Angular 6", "Bootstrap 4", "TypeScript"],
     },
-    {
-      company: "E-Tech Service Pvt. Ltd.",
-      title: "MEAN Stack Developer",
-      timePeriod: "1.2020 - Present",
-      mainTechStack: ["Angular 6,7,8"],
-      technologies: ["Angular 8", "LoopBack 3.x", "TypeScript"],
-    },
   ];
-
+  const MainTechStack = (item) => {
+    return (
+      <div>
+        {item.map((tech, techIndex) => (
+          <span key={techIndex}>
+            <Badge variant="primary" className={"badge"}>{tech}</Badge>
+          </span>
+        ))}
+      </div>
+    );
+  };
+  
+  const TechnologyBadges = (item) => {
+    return (
+      <div>
+        {item.map((tech, techIndex) => (
+          <span key={techIndex}>
+            <Badge variant="light" className={"badge"}>{tech}</Badge>
+          </span>
+        ))}
+      </div>
+    );
+  };
+  const RandomColour = () => {
+    return "#" + ((1<<24)*Math.random() | 0).toString(16)
+  };
   return (
-    <section>
+    <section className={"main_section"}>
+       <h1 className="section_title">
+          Work <span>Experience</span>
+        </h1>
       <VerticalTimeline>
-        {resumeExperience.map((item, i) =>
-          item.technologies.map((tech, techIndex) => (
-            <div key={techIndex}>
-              <div>
-                <Badge variant="light">{tech}</Badge>
+        {resumeExperience.map((item, i) => (
+            <VerticalTimelineElement
+            key={i}
+              className="vertical-timeline-element--work"
+              date={item.timePeriod}
+              iconStyle={{
+                background: `${RandomColour()}`,
+                color: "#fff",
+                textAlign: "center",
+              }}
+              icon={
+                item.icon ? <i className={`${item.icon} tech_icon`}></i> : <img src={item.image} className={"tech_images"} alt={item.image} />
+            //   <i className="fab fa-angular experience-icon"></i>
+            }
+            >
+              <div style={{ textAlign: "left", marginBottom: "4px" }}>
+                {MainTechStack(item.mainTechStack)}
               </div>
-              <div>{item.company}</div>
-            </div>
-          ))
-        )}
 
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="12-12"
-          iconStyle={{
-            background: "#AE944F",
-            color: "#fff",
-            textAlign: "center",
-          }}
-          icon={<i className="fab fa-angular experience-icon"></i>}
-        >
-          <div style={{ textAlign: "left", marginBottom: "4px" }}>
-            {"Angular"}
-          </div>
-
-          <h3
-            className="vertical-timeline-element-title"
-            style={{ textAlign: "left" }}
-          >
-            {"Angular Dev"}
-          </h3>
-          <h4
-            className="vertical-timeline-element-subtitle"
-            style={{ textAlign: "left" }}
-          >
-            {"E-Tech"}
-          </h4>
-          <div style={{ textAlign: "left", marginTop: "15px" }}>
-            {"Angular"}
-          </div>
-        </VerticalTimelineElement>
+              <h3
+                className="vertical-timeline-element-title"
+                style={{ textAlign: "left" }}
+              >
+                {item.position}
+              </h3>
+              <h4
+                className="vertical-timeline-element-subtitle"
+                style={{ textAlign: "left" }}
+              >
+                {item.company}
+              </h4>
+              <div style={{ textAlign: "left", marginTop: "15px" }}>
+                {TechnologyBadges(item.technologies)}
+              </div>
+            </VerticalTimelineElement>
+        ))}
       </VerticalTimeline>
     </section>
   );
