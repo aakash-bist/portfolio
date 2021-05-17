2c9998704591679350f606b3fb5866f07d7647c7
aakash <aakash@etech-services.com>
2021-05-18 00:56:29 +0530

    cleaned up

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
 // import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume/Resume";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ScrollToTop from "./components/ScrollToTop";
 function App() {
-  const [load, upadateLoad] = useState(true);
+  const [load, setLoad] = useState(true);
   useEffect(() => {
     setTimeout(() => {
-      upadateLoad(false);
+      setLoad(false);
     }, 1200);
   }, []);
   return (
@@ -26,9 +23,6 @@ function App() {
         <ScrollToTop />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/project" component={Projects} />
-          <Route path="/about" component={About} />
-          <Route path="/resume" component={Resume} />
         </Switch>
         <Footer />
       </div>
